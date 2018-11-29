8dc153a8fdf7c92416c20c09d768267be77bad10
Sam <sam@daredata.engineering>
2018-11-29 17:37:57 +0000

    added a new role

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snowconn',
-    version='2.1.0',
+    version='2.2.0',
     description='Python utilities for connection to Daltix snowflake data '
                 'source',
     # url='TODO',
