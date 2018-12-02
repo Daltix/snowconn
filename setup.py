c9b8c54c2958865a143c3dcdff564d1bed37a57d
Sam <sam@daredata.engineering>
2018-12-02 15:14:20 +0000

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snowconn',
-    version='3.0.0',
+    version='3.0.1',
     description='Python utilities for connection to Daltix snowflake data '
                 'source',
     # url='TODO',
