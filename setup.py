8518e56761f1162b4b30fc01bf6915aa28f1fa5b
Sam Hopkins <sam@daredata.engineering>
2018-12-12 15:29:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snowconn',
-    version='3.1.0',
+    version='3.2.0',
     description='Python utilities for connection to Daltix snowflake data '
                 'source',
     # url='TODO',
