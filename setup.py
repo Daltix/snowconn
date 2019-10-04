4d2e0513ed3ddf6d9be0914062dc3cd1bbf3ae27
Charlie Orford <charlie.orford@daltix.com>
2019-10-04 13:05:22 +0000

    Updated snowflake-sqlalchemy dependency (to resolve https://github.com/snowflakedb/snowflake-connector-python/pull/210)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(readme_path, encoding='utf-8') as fh:
 
 setup(
     name='snowconn',
-    version='3.5.9',
+    version='3.5.10',
     description='Python utilities for connection to the Snowflake data '
                 'warehouse',
     url='https://github.com/Daltix/snowconn',
@@ -20,7 +20,7 @@ setup(
     packages=['snowconn'],
     install_requires=[
         'wheel==0.32.3',
-        'snowflake-sqlalchemy==1.1.14',
+        'snowflake-sqlalchemy==1.1.15',
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
