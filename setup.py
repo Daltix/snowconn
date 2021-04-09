77268c65f0dc351289c53ef88ceb98b4117211b2
Simon Esprit <simon.esprit@daltix.com>
2021-04-09 10:29:35 +0100

    Bump snowflake-sqlalchemy version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=['snowconn'],
     install_requires=[
         'wheel==0.32.3',
-        'snowflake-sqlalchemy==1.2.3',
+        'snowflake-sqlalchemy==1.2.4',
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
